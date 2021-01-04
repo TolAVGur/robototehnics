d8647be25d21928d94871e479389731e2f59fdda
Anatolii <gurov.anatoliy@gmail.com>
2021-01-04 18:45:09 +0200

    Исправил ссылки в контактах и на главной + ускорил выпадающий список

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@ $(document).ready(function () {
 
     $('#nav2 ul li').hoverIntent(
         function () {
-            $(this).find('ul').slideDown(600);
+            $(this).find('ul').slideDown(50);
         },
         function () {
-            $(this).find('ul').slideUp(600);
+            $(this).find('ul').slideUp(50);
         }
     );
 
